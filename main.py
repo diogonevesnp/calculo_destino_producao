d2c1c1edc5d7f19f52a93f16a76f222a46886a5f
diogonevesnp <diogonevesnp@gmail.com>
2025-11-21 14:31:51 -0300

    adicionado o tratamento de erros

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+#função de válidação
+def ler_numero(mensagem):
+    while True:
+        try:
+            return int(input(mensagem))
+        except ValueError:
+            print("❌ Erro: Entrada inválida. Digite apenas números inteiros.")
+
 # Função para realização dos cálculos de divisão para cada tipo de produção, envase/expedição.
 def quantidade_correta (opcao_linha, quantidade_producao, peso_bag_sacaria_expedicao):
     if opcao_linha == 1:
@@ -29,10 +37,11 @@ def formata_br(valor, casas_decimais=2, dividir_por_mil=False):
     formato = f"{valor:,.{casas_decimais}f}"
     formato = formato.replace(",", "X").replace(".", ",").replace("X", ".")
     return formato
-   
-# Menu de opções
-print('\n∷ MENU QUANTIDADE CORRETA POR DESTINO:\n')
 
+# Menu de opções
+print ('-' * 40)
+print('∷ MENU QUANTIDADE CORRETA POR DESTINO:')
+print ('-' * 40)
 print('[1] Bag Ruminantes')
 print('[2] Bag Pet/Peixes')
 print('[3] Sac Ruminates')
@@ -42,59 +51,75 @@ print('[6] Exp Granel Ruminantes')
 print('[7] Exp Granel Aves/suinos')
 print('[8] Exp Granel Pet/PEixes')
 print('[9] Envase Cães')
-print('[10] Carregamento Expedição\n')
+print('[10] Carregamento Expedição')
+print ('-' * 30)
+
+
+# Loop para tratamento de erros adicionado
+while True:
+    opcao_linha = ler_numero ('Digite uma opção de 1 a 10: ')
+    if 1 <= opcao_linha <= 10:
+        break
+    else:
+         print("❌ Opção inválida. Escolha entre 1 e 10.")
+
+quantidade_producao = 0
+peso_bag_sacaria_expedicao = 1
 
 # Condição de escolha e filtro
-opcao_linha = int(input('Digite uma opção: '))
 if opcao_linha in (1, 2):
-    quantidade_producao = int(input('Quantas Bateladas?: '))
-    peso_bag_sacaria_expedicao = int(input('Qual o peso de cada bag?: '))
+    quantidade_producao = ler_numero ('Quantas Bateladas?: ')
+    peso_bag_sacaria_expedicao = ler_numero ('Qual o peso de cada bag?: ')
 elif opcao_linha in (3, 4):
-    quantidade_producao = int(input('Qunatas bateladas?: '))
-    peso_bag_sacaria_expedicao = int(input('Qual o peso de cada sacaria?: '))
+    quantidade_producao = ler_numero ('Qunatas bateladas?: ')
+    peso_bag_sacaria_expedicao = ler_numero ('Qual o peso de cada sacaria?: ')
 elif opcao_linha == 5:    
-  quantidade_producao = int(input('Qunatas bateladas?: '))
-  peso_bag_sacaria_expedicao = int(input('Qual o peso de cada sacaria?: '))
+  quantidade_producao = ler_numero ('Qunatas bateladas?: ')
+  peso_bag_sacaria_expedicao = ler_numero ('Qual o peso de cada sacaria?: ')
 elif opcao_linha == 6:
-  quantidade_producao = int(input('Qunatas bateladas?: '))
+  quantidade_producao = ler_numero ('Qunatas bateladas?: ')
   peso_bag_sacaria_expedicao = 1
 elif opcao_linha == 7:
-  quantidade_producao = int(input('Qunatas bateladas?: '))
+  quantidade_producao = ler_numero ('Qunatas bateladas?: ')
 elif opcao_linha == 8:  
-  quantidade_producao = int(input('Qunatas bateladas?: '))
+  quantidade_producao = ler_numero ('Qunatas bateladas?: ')
 elif opcao_linha == 9:
-  quantidade_producao = int(input('Qunatas bateladas?: '))
-  peso_bag_sacaria_expedicao = int(input('Qual o peso de cada pacote?: '))
+  quantidade_producao = ler_numero ('Qunatas bateladas?: ')
+  peso_bag_sacaria_expedicao = ler_numero ('Qual o peso de cada pacote?: ')
 elif opcao_linha == 10:
-  quantidade_producao = int(input('Quantidade a ser carregada?: '))
-  peso_bag_sacaria_expedicao = int(input('Quantas caixas?: '))  
+  quantidade_producao = ler_numero ('Quantidade a ser carregada?: ')
+  peso_bag_sacaria_expedicao = ler_numero ('Quantas caixas?: ')  
 else:
     print('Opção inválida')  
 
-# Armazenamento do resultado da função
-resultado = quantidade_correta (opcao_linha, quantidade_producao, peso_bag_sacaria_expedicao)
 
-# Mostrando o resultado para o usuário
-if opcao_linha == 1:
-    print(f'Em uma produção de {quantidade_producao} bateladas na linha de Ruminates, o total a ser retirado do silo 80701 é de {formata_br(resultado)} Bags')
-elif opcao_linha == 2:
-    print(f'Em uma produção de {quantidade_producao} bateladas na linha Pet/Peixes, o total de Big Bag no silo 90907 é de {formata_br(resultado)}')
-elif opcao_linha == 3:
-    print(f'Em uma produção de {quantidade_producao} bateladas na linha Ruminates, o total de Sacarias a ser tirado é de {formata_br(resultado)}')
-elif opcao_linha == 4:
-    print(f'Em uma produção de {quantidade_producao} bateladas na linha Aves/Suinos, o total de Sacarias a ser tirado é de {formata_br(resultado)}')
-elif opcao_linha == 5:
-    print(f'Em produção de {quantidade_producao} bateladas na linha Pet/Peixes, o total de Sacarias a ser tirado é de {formata_br(resultado)}') 
-elif opcao_linha == 6:
-    print(f'Em uma produção de {quantidade_producao} bateladas na linha Ruminates, o total para o Granel Ruminates é de {formata_br(resultado)}')
-elif opcao_linha == 7:
-    print(f'Em uma produção de {quantidade_producao} batelasas na linha Aves/suinos, o total para o Ganel Aves/Suinos é de {formata_br(resultado)}')  
-elif opcao_linha == 8:
-    print(f'Em uma produção de {quantidade_producao} bateladas na linha Pet/Peixes, o total para o Granel Pet/Peixes é de {formata_br(resultado)}')
-elif opcao_linha == 9:
-    print(f'Em uma produção de {quantidade_producao} bateladas na linha Pet/Peixes (ração de cães), o total de Pacotes a ser tirado é de {formata_br(resultado)}')
-elif opcao_linha == 10:
-    print(f'O caminhão deve ser carregado desta forma, {formata_br(resultado)} em cada caixa.')                            
-else:
-    print('Batelada não reconhecida')
+try:
+    # Armazenamento do resultado da função
+    resultado = quantidade_correta (opcao_linha, quantidade_producao, peso_bag_sacaria_expedicao)
+
+    # Mostrando o resultado para o usuário
+    if opcao_linha == 1:
+      print(f'Em uma produção de {quantidade_producao} bateladas na linha de Ruminates, o total a ser retirado do silo 80701 é de {formata_br(resultado)} Bags')
+    elif opcao_linha == 2:
+      print(f'Em uma produção de {quantidade_producao} bateladas na linha Pet/Peixes, o total de Big Bag no silo 90907 é de {formata_br(resultado)}')
+    elif opcao_linha == 3:
+      print(f'Em uma produção de {quantidade_producao} bateladas na linha Ruminates, o total de Sacarias a ser tirado é de {formata_br(resultado)}')
+    elif opcao_linha == 4:
+      print(f'Em uma produção de {quantidade_producao} bateladas na linha Aves/Suinos, o total de Sacarias a ser tirado é de {formata_br(resultado)}')
+    elif opcao_linha == 5:
+      print(f'Em produção de {quantidade_producao} bateladas na linha Pet/Peixes, o total de Sacarias a ser tirado é de {formata_br(resultado)}') 
+    elif opcao_linha == 6:
+      print(f'Em uma produção de {quantidade_producao} bateladas na linha Ruminates, o total para o Granel Ruminates é de {formata_br(resultado)}')
+    elif opcao_linha == 7:
+      print(f'Em uma produção de {quantidade_producao} batelasas na linha Aves/suinos, o total para o Ganel Aves/Suinos é de {formata_br(resultado)}')  
+    elif opcao_linha == 8:
+      print(f'Em uma produção de {quantidade_producao} bateladas na linha Pet/Peixes, o total para o Granel Pet/Peixes é de {formata_br(resultado)}')
+    elif opcao_linha == 9:
+      print(f'Em uma produção de {quantidade_producao} bateladas na linha Pet/Peixes (ração de cães), o total de Pacotes a ser tirado é de {formata_br(resultado)}')
+    elif opcao_linha == 10:
+      print(f'O caminhão deve ser carregado desta forma, {formata_br(resultado)} em cada caixa.')                            
+    else:
+      print('Batelada não reconhecida')
+except Exception as e:
+  print(f"\n❌ Ocorreu um erro inesperado durante o cálculo: {e}")
 print() 
